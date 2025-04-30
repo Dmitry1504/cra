e656f8889cc9e258bf8b46a0515a03c48de15029
rubdmitry <xxxdemongtxxx@gmail.com>
2025-04-30 11:48:28 +0300

    cra

---
+++ my-react-app/src/styles/Theme.styled.tsx
@@ -0,0 +1,15 @@
+export const myTheme = {
+    colors: {
+        primary: 'gold',
+        secondary: 'lightgreen',
+        grey: {
+            dark: '#000000',
+            light: '#ffffff',
+        }
+    },
+
+    media: {
+        tablet: 'screen and (max-width: 768px)',
+        mobile: 'screen and (max-width: 576px)',
+    }
+}
